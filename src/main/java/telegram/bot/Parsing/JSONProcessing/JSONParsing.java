2d6ee395da19b4612a1edbd5ed0e7a791a22d483
Artem Bakanov <artembakanov123@yandex.ru>
2020-12-07 20:45:08 +0300

    Deploying Telegram bot

---
+++ src/main/java/telegram/bot/Parsing/JSONProcessing/JSONParsing.java
@@ -0,0 +1,77 @@
+package telegram.bot.Parsing.JSONProcessing;
+
+import com.google.gson.stream.JsonReader;
+
+import java.net.HttpURLConnection;
+import java.util.List;
+
+public abstract class JSONParsing {
+    Double lon, lat, speed, temp, max_temp, min_temp;
+    Integer id, press, humid, clouds, deg;
+    String city, country;
+    List<String> weather;
+
+    public JSONParsing() {}
+
+    public void setLat(Double lat) { this.lat = lat; }
+
+    public void setLon(Double lon) { this.lon = lon; }
+
+    public void setCity(String name) { this.city = name; }
+
+    public void setId(Integer id) { this.id = id; }
+
+    public void setCountry(String country) { this.country = country; }
+
+    public void setClouds(Integer clouds) { this.clouds = clouds; }
+
+    public void setSpeed(Double speed) { this.speed = speed; }
+
+    public void setDeg(Integer deg) { this.deg = deg; }
+
+    public void setHumid(Integer humid) { this.humid = humid; }
+
+    public void setTemp(Double temp) { this.temp = temp; }
+
+    public void setMax_temp(Double max_temp) { this.max_temp = max_temp; }
+
+    public void setMin_temp(Double min_temp) { this.min_temp = min_temp; }
+
+    public void setPress(Integer press) { this.press = press; }
+
+    public void setWeather(List<String> weather) { this.weather = weather; }
+
+    public Double getLat() { return lat; }
+
+    public Double getLon() { return lon; }
+
+    public String getCity() { return city; }
+
+    public Integer getId() { return id; }
+
+    public String getCountry() { return country; }
+
+    public Double getSpeed() { return speed; }
+
+    public Integer getDeg() { return deg; }
+
+    public Double getTemp() { return temp; }
+
+    public Double getMax_temp() { return max_temp; }
+
+    public Double getMin_temp() { return min_temp; }
+
+    public Integer getClouds() { return clouds; }
+
+    public Integer getHumid() { return humid; }
+
+    public Integer getPress() { return press; }
+
+    public List<String> getWeather() { return weather; }
+
+    public abstract void readJSON(HttpURLConnection urlConnection);
+
+    public abstract void readJSONSubParams(JsonReader jsonReader);
+
+    public abstract void readJSONWeather(JsonReader jsonReader);
+}
---
+++ src/main/java/telegram/bot/Parsing/JSONProcessing/JSONParsingCurrentWeather.java
@@ -0,0 +1,103 @@
+package telegram.bot.Parsing.JSONProcessing;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.stream.JsonReader;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class JSONParsingCurrentWeather extends JSONParsing {
+
+    public JSONParsingCurrentWeather() {}
+
+    @Override
+    public void readJSON(HttpURLConnection urlConnection) {
+        try {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonReader jsonReader = gson.newJsonReader(new InputStreamReader(urlConnection.getInputStream(), StandardCharsets.UTF_8));
+            jsonReader.beginObject();
+            while (jsonReader.hasNext()) {
+                String name = jsonReader.nextName();
+                switch (name) {
+                    case "name" -> city = jsonReader.nextString();
+                    case "id" -> id = jsonReader.nextInt();
+                    case "coord", "main", "clouds", "sys", "wind" -> readJSONSubParams(jsonReader);
+                    case "weather" -> readJSONWeather(jsonReader);
+                    default -> jsonReader.skipValue();
+                }
+            }
+            jsonReader.endObject();
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+
+    @Override
+    public void readJSONSubParams(JsonReader jsonReader) {
+        try {
+            jsonReader.beginObject();
+            while (jsonReader.hasNext()) {
+                String name = jsonReader.nextName();
+                switch (name) {
+                    case "lon" -> lon = jsonReader.nextDouble();
+                    case "lat" -> lat = jsonReader.nextDouble();
+                    case "temp" -> temp = jsonReader.nextDouble() - 273.15;
+                    case "temp_min" -> min_temp = jsonReader.nextDouble() - 273.15;
+                    case "temp_max" -> max_temp = jsonReader.nextDouble() - 273.15;
+                    case "pressure" -> press = jsonReader.nextInt();
+                    case "humidity" -> humid = jsonReader.nextInt();
+                    case "all" -> clouds = jsonReader.nextInt();
+                    case "country" -> country = jsonReader.nextString();
+                    case "speed" -> speed = jsonReader.nextDouble();
+                    case "deg" -> deg = jsonReader.nextInt();
+                    default -> jsonReader.skipValue();
+                }
+            }
+            jsonReader.endObject();
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+
+    @Override
+    public void readJSONWeather(JsonReader jsonReader) {
+        try {
+            weather = new ArrayList<>();
+            jsonReader.beginArray();
+            while (jsonReader.hasNext()) {
+                jsonReader.beginObject();
+                while (jsonReader.hasNext()) {
+                    String name = jsonReader.nextName();
+                    if (name.equals("description")) weather.add(jsonReader.nextString());
+                    else jsonReader.skipValue();
+                }
+                jsonReader.endObject();
+            }
+            jsonReader.endArray();
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+
+    @Override
+    public String toString() {
+        String s = "Город: " +
+                city + "(" + id + ")\n" +
+                "Координаты: " +
+                "Широта: " + lat + "; " +
+                "Долгота: " + lon + "\n" +
+                "Страна: " + country + "\n" +
+                "Погода: ";
+        for (String weath: weather)
+            s = s + weath + ",";
+        s = s.substring(0, s.length() - 1);
+        s = s  + "\n" +
+                "Температура: " + String.format("%.2f", temp) + " °C\n" +
+                "Минимальная температура: " + String.format("%.2f", min_temp) + " °C\n" +
+                "Максимальная температура: " + String.format("%.2f", max_temp) + " °C\n" +
+                "Влажность: " + humid + "%\n" +
+                "Давление: " + press + " hPa\n" +
+                "Ветер: " + speed + " м/с, " + deg + " градусов" + "\n" +
+                "Облачность: " + clouds + "%\n" +
+                "Текущее время: " + new Date().toString();
+        return s;
+    }
+}
